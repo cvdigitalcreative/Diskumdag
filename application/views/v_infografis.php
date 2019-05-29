1c4f2aa370076b47032755550bf4f9300a06fcde
fuadwahid <fuadwahid91@gmail.com>
2019-05-29 23:13:46 +0700

    margin

--- application/views/v_infografis.php
+++ application/views/v_infografis.php
@@ -10,7 +10,7 @@
             <div class="row mt-4 mb-6">
                 <div class="col">
                     <div class="col-lg-12">
-                        <h4><?= $judul ?></h4>
+                        <h4 style="margin-top: 50px;"><?= $judul ?></h4>
                         <div class="col-md-12" style="margin-bottom: 20px;">
                             <img src="<?= base_url() ?>\assets\images\<?= $gambar ?>" width="100%">
                         </div>
